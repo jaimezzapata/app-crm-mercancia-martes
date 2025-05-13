faff11efed22ad4ac1447f3cbb85d3745e8af0c6
jaimezzapata <zapataval2304@gmail.com>
2025-05-13 10:26:58 -0500

    Filtrado de envios

--- src/components/Contenido.jsx
+++ src/components/Contenido.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+let apiEnvios = "https://back-json-server-martes.onrender.com/envios";
+
 const Contenido = () => {
+  const [envios, setEnvios] = useState([])
+  let usuarioLogueado = JSON.parse(localStorage.getItem("usuario"))
+  function getEnvios() {
+    fetch(apiEnvios)
+      .then((response) => response.json())
+      .then((data) => setEnvios(data))
+      .catch((error) => console.log(error));
+  }
+  useEffect(() => {
+    getEnvios();
+  }, [])
+
+  function filtrarEnviosUsuario(){
+    let enviosFiltrados = envios.filter((item)=> item.usuarioId == usuarioLogueado.id)
+    console.log(enviosFiltrados)    
+  }
+  
   return (
     <section className="aplicacion__informacion">
-     Hola
+
     </section>
   )
 }
